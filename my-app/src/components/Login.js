3f2c8cf2deea29ed0c90588c8f4533f60f3dd7c0
MojtabaNabid <[mojtaba.nabid@gmail.com]>
2024-09-24 17:51:45 +0200

    get information from user and store it into the formdata useState

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -9,22 +9,37 @@ import logo from '../assets/imgages/wallet-logo.jpg'
 import { Row, Col, Container } from 'react-bootstrap';
 
 function Login() {
+
+    const [formData, setformData] = useState({
+        username: "",
+        password: ""
+    })
+
+    function handleSumbit(e) {
+        e.preventDefault();
+        console.log(formData);
+    }
+
     return (
         <div className="App">
           <Container fluid className="vh-100">
             <Row className='h-100 d-flex align-items-center justify-content-center '>
               <Col>
-                <Form className='login-form p-4 shadow'>
+                <Form className='login-form p-4 shadow' onSubmit={handleSumbit}>
                   <img src={logo} className='logo' alt="logo" />
                   <Form.Group className="mb-4" controlId="formBasicEmail">
                     <FloatingLabel controlId='floatingUsername' label="Username" className='mb-3'>
-                      <Form.Control type="email" placeholder="Username or Eamil address" />
+                      <Form.Control type="email" 
+                                    placeholder="Username or Eamil address" 
+                                    onChange={e => setformData({...formData, username: e.target.value})}/>
                     </FloatingLabel>
                   </Form.Group>
     
                   <Form.Group className="mb-3" controlId="formBasicPassword">
                     <FloatingLabel controlId='floatingPassword' label="Password" className='mb-3'>
-                      <Form.Control type="password" placeholder="Password" />
+                      <Form.Control type="password" 
+                                    placeholder="Password"
+                                    onChange={e => setformData({...formData, password: e.target.value})} />
                     </FloatingLabel>
                   </Form.Group>
     
