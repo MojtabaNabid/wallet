e6cb37743035d64acaf67d1d38713903384e7ccd
MojtabaNabid <[mojtaba.nabid@gmail.com]>
2024-11-22 18:40:21 +0100

    adding logo image

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import Login from './components/Login'
 import Home from './components/Home'
+import Nav from './components/Nav'
 import Confirmation_Login from './components/Confirmation_Login';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -13,6 +14,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/Nav' element={<Nav />}/>
           <Route path='/login' element={<Login />} />
           <Route path='/confirmation_login' element={<Confirmation_Login />} />
         </Routes>
--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,5 +1,9 @@
 function Nav() {
-
+return (
+    <>
+        <img src="../assets/image/logo-bg-white-1.jpg" alt="My Wallet Logo" />
+    </>
+)
 }
 
 export default Nav;
